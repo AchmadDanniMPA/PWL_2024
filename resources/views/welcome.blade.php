e9dd30451d088311dee3f07b81a2b894ca10fc7f
AchmadDanniMPA <gundul321mmm@gmail.com>
2024-08-29 19:19:14 +0700

    Tugas Menambahkan Info KTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,54 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>PWL Danni Per1 2024</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+            }
+            .ktp-box {
+                width: 400px;
+                margin: 0 auto;
+                padding: 20px;
+                border: 2px solid black;
+                border-radius: 10px;
+                text-align: left;
+                background-color: #f9f9f9;
+            }
+            .ktp-box h3 {
+                text-align: center;
+                margin: 0;
+            }
+            .ktp-box p {
+                margin: 5px 0;
+            }
+            .indent {
+                text-indent: 20px;
+            }
+            .tabbed {
+                display: inline-block;
+                margin-left: 20px;
+            }
+        </style>
     </head>
     <body>
         <h2>Pemrograman Web Lanjut 2024</h2>
         <h3>Semangat Untuk Farming Ilmu Laravel 10</h3>
+        <div class="ktp-box">
+            <h3>KTP</h3>
+            <p style="text-align: center; margin-bottom: 20px;">PROVINSI JAWA TIMUR<br>KABUPATEN LUMAJANG</p>
+            <p style="margin-bottom: 10px;">NIK: 3509220403005001</p>
+            <p>Nama: Achmad Danni Muchtarom Putra Alamsyah</p>
+            <p>Tempat/Tgl Lahir: LUMAJANG, 04-04-2004</p>
+            <p>Jenis Kelamin: LAKI-LAKI <span class="tabbed">Gol. Darah: -</span></p>
+            <p>Alamat: DUSUN GENTENGSARI</p>
+            <p class="indent">RT/RW: 004/012</p>
+            <p class="indent">Kel/Desa: PULO</p>
+            <p class="indent">Kecamatan: TEMPEH</p>
+            <p>Agama: ISLAM</p>
+            <p>Status Perkawinan: BELUM KAWIN</p>
+            <p>Pekerjaan: PELAJAR/MAHASISWA</p>
+            <p>Kewarganegaraan: WNI</p>
+            <p>Berlaku Hingga: SEUMUR HIDUP</p>
+        </div>
     </body>
 </html>
