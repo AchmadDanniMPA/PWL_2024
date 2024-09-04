84a688726b16b33e7f4be018bc1faaaf3283a47c
AchmadDanniMPA <gundul321mmm@gmail.com>
2024-09-04 14:37:17 +0700

    Per 2 Prak 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'Za World';
+});
+
+Route::get('/selamatdatang', function(){
+    return 'Selamay De Datang Di';
+});
+
+Route::get('/about', function(){
+    return 'NIM 2241760095 <br> Nama Achmad Danni Muchtarom Putra Alamsyah';
+});
+
+Route::get('/user/{name?}',function($name=null){
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}',function($postId, $commentId){
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}',function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
